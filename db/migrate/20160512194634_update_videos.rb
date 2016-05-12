b8d2a27052a7ca9ef7e02c98746526aab3090fe1
Jacob Walker <jacobwalker0814@gmail.com>
2016-05-12 17:08:17 -0500

    Fix migration for postgres

--- db/migrate/20160512194634_update_videos.rb
+++ db/migrate/20160512194634_update_videos.rb
@@ -1,9 +1,17 @@
 class UpdateVideos < ActiveRecord::Migration
-  def change
+  def up
     add_column :videos, :ytID, :string
     add_column :videos, :author, :string
-    remove_column :videos, :content, :text
-    remove_column :videos, :link, :text
-    remove_column :videos, :guid, :text
+    remove_column :videos, :content
+    remove_column :videos, :link
+    remove_column :videos, :guid
+  end
+
+  def down
+    remove_column :videos, :ytID
+    remove_column :videos, :author
+    add_column :videos, :content, :text
+    add_column :videos, :link, :text
+    add_column :videos, :guid, :text
   end
 end
